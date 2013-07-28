bd0ff4e866e2b5f9ac651ff2c851c2558a162161
Fandant <kengo475@kelbin55.local>
2013-07-28 09:28:14 +0900

    hash value calcualte
    MD5,SHA1,SHA256 of one file

---
+++ get_hash.py
@@ -0,0 +1,26 @@
+# -*-  coding: utf-8 -*-
+
+import sys
+import os
+import hashlib
+
+argvl = sys.argv
+argc = len(argvl)
+if argc != 2 :
+	print "usage:python %s filenme" % argvl[0]
+	quit()
+
+fsize = os.path.getsize(argvl[1])
+if fsize > 4096*1024**3:
+	print "can not calclate hash"
+	quit()
+	
+f = open(argvl[1])
+cont = f.read()
+
+print "file path:" + os.path.abspath(argvl[1])
+print ("file size:%sbyte" % (fsize))
+print "MD5:\t" + hashlib.md5(cont).hexdigest()
+print "SHA1:\t" + hashlib.sha1(cont).hexdigest()
+print "SHA256:\t" + hashlib.sha256(cont).hexdigest()
+
